c67bc83e9f11aa97c5abfb57dc4a7b14d49d0270
Tonett0 <153796277+Tonett0@users.noreply.github.com>
2023-12-14 15:33:06 +0800

    初始專案

---
+++ js-tutorial-main/02 dom/red-yellow-green/begin/script.js
@@ -0,0 +1,44 @@
+/* 🚀🔥
+黃綠紅 你要哪一個？
+
+此專案涵蓋的概念
+- DOM 操作
+- 事件監聽器
+- 迴圈 forEach 
+- 變數
+- 條件語句 (if else if)
+- 樣板字面值
+*/
+
+
+// 將 title 文字改成 '報告阿柴，標題已經切換！' 並將字體顏色改成藍色
+// function changeTitle(){
+  // let titleDiv = document.getElementById("title")
+  // console.log("title", titleDiv)
+  // let message = '標題切換'
+  // titleDiv.innerText = "title"+message
+  // titleDiv.innerHTML = `<p>${message}</p>`
+  // titleDiv.style.color = 'blue'
+// }
+
+
+// const squares = document.querySelectorAll('.colorSquare')
+// const output = document.getElementById('output')
+// // 將每個元素綁定
+// squares.forEach(square => {
+//   square.onclick = () => {
+//     _id = square.getAttribute('id')
+//     console.log(_id);
+//     output.innerHTML =`<div class='colorSquare' id="${_id}" ></div>`
+//   }
+// })
+
+
+function resetScore() {
+
+}
+
+const resetGameBtn = document.getElementById('reset')
+// 綁定 reset 事件
+resetGameBtn.onclick = () => resetScore()
+
---
+++ script.js
@@ -0,0 +1,32 @@
+//內建方法介紹console.log() => 在控制台中印出
+
+// console.log("hello, js")
+// console.log("hi, js")
+
+// //變數宣告( var、const、let)
+
+// let name = "codeshiba"
+// console.log(name)
+
+// let sentence = '今天過得如何? 很高興見到， 希望妳有美好的一天'
+
+// console.log(name+sentence)
+
+
+// //prompt()彈出輸入視窗
+// fruit = prompt("你最喜歡的水果是甚麼?")
+// console.log(fruit)   //主控台輸出fruit內容
+
+//alert()彈出輸入視窗+顯示輸入內容
+// fruit = prompt("你最喜歡的水果是甚麼?")
+// alert(fruit)  //彈出視窗輸出fruit內容
+
+
+//小費計算
+// let food = parseInt(prompt("消費金額"))
+// let tipPercentage =  parseInt(prompt("小費幾趴"))/100
+// let tipAmount = food * tipPercentage
+// let total = food+tipAmount
+// alert(total)
+
+
